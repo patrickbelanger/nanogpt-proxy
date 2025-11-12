c358a329e387f395954675282eedbc1e0844aa1c
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-11-11 22:34:50 -0500

    chore(proxy-refactor): fix gitaction backend

--- proxy/jest.config.js
+++ proxy/jest.config.js
@@ -1,4 +1,7 @@
 export default {
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageReporters: ["json-summary", "text", "lcov"],
   testEnvironment: 'node',
   transform: {},
   setupFiles: ['dotenv/config'],
