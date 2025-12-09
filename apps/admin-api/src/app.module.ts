ab0634034cde0d9202806d0d2c5b9bc8f4fe828c
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-12-08 23:14:08 -0500

    fix(docker-image-build): added health check

--- apps/admin-api/src/app.module.ts
+++ apps/admin-api/src/app.module.ts
@@ -3,15 +3,18 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import { SecurityModule } from './security/security.module';
+import { HealthModule } from '@nanogpt-monorepo/core';
 
 @Module({
   imports: [
+    HealthModule.register({ serviceName: 'admin-api' }),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
     AuthModule,
     UsersModule,
     SecurityModule,
+    HealthModule,
   ],
 })
 export class AppModule {}
