5980588b314ca61d4cfb337159917694876af5fe
Patrick Bélanger <1848500+patrickbelanger@users.noreply.github.com>
2025-12-08 23:43:00 -0500

    fix(docker-image-build): replace console.log/error with logger

--- apps/proxy/src/forwarder/forwarder.controller.ts
+++ apps/proxy/src/forwarder/forwarder.controller.ts
@@ -1,17 +1,18 @@
-import { Controller, All, Req, Res, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, All, Req, Res, HttpException, HttpStatus, Logger } from '@nestjs/common';
 import express from 'express';
 import { ForwarderService } from './forwarder.service';
 
 @Controller('/v1')
 export class ForwarderController {
   constructor(private readonly forwarder: ForwarderService) {}
+  private readonly logger = new Logger(ForwarderController.name);
 
   @All('*')
   async proxy(@Req() req: express.Request, @Res() res: express.Response) {
     try {
       await this.forwarder.handleRequest(req, res);
     } catch (err: unknown) {
-      console.error('Forwarder error:', err);
+      this.logger.error('Forwarder error:', err);
 
       if (err instanceof HttpException) {
         throw err;
