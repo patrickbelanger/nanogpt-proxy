3c5297b59e95fdf1440a2f240a557fcbbb8aab3a
symphonic-navigator <navigator4223@gmail.com>
2025-12-13 21:58:57 +0100

    increased request size

--- apps/proxy/src/main.ts
+++ apps/proxy/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { EnvironmentService } from '@nanogpt-monorepo/core';
+import { json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const environmentService = app.get(EnvironmentService);
+
+  app.use(json({ limit: '64mb' }));
 
   await app.listen(environmentService.proxyPort);
 }
